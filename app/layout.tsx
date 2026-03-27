91fbec86d9d07b209e512aa7c3873a463e792b2c
Quinton <quintontram10@gmail.com>
2026-03-27 17:18:16 -0400

    add: mobile navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "Arbor Financial",
   description: "Arbor is a banking platform that allows users to freely access, transfer, and manage money",
   icons: {
-    icon: '/icons/logo.png'
+    icon: '/icons/logo1.v2.png'
   }
 };
 
